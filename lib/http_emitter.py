b040b2956f9d84a63323a2d66e7cf4e5a232fd76
Jordan Griveau <jgriveau@yahoo.fr>
2022-08-14 22:00:29 +0200

    Adding print of status_code + index while generating logs

--- lib/http_emitter.py
+++ lib/http_emitter.py
@@ -6,11 +6,13 @@ import lib.log_generator as log_generator
 
 ENDPOINT = "http://127.0.0.1:3000"
 
-for _ in range(1000):
+for i in range(1000):
     try:
-        requests.post(
+        req = requests.post(
             ENDPOINT, json={"log": log_generator.sample(uuid.uuid4())}, timeout=0.1
         )
+
+        print(f"{req.status_code} {i + 1}/1000")
     except requests.Timeout:
         print(f"Remote server timed-out on {ENDPOINT}")
     except requests.ConnectionError:
